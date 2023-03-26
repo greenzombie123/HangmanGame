3bca6d2459962b62fef7c18f5d684a292baef296
Alvin Hart <pageking123@gmail.com>
2023-03-26 12:20:45 +0900

    Implement addLetters function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Dictionary from "./modules/Dictionary";
 import Model from "./modules/Model";
 import Hangman from "./modules/Hangman";
-// import { createElement, getElement } from "./modules/Helpers";
+import View from "./modules/View";
+import { createElement, getElement } from "./modules/Helpers";
 
 const hangman = new Hangman()
-hangman.draw()
+// hangman.draw()
+const view = new View(hangman)
 const model = new Model(Dictionary)
 console.log(model);
+view.createTiles()
+view.addLetters('a')
--- src/modules/View.js
+++ src/modules/View.js
@@ -1,9 +1,31 @@
-import { createElement, getElement } from "./modules/Helpers";
+import { createElement, getElement } from "./Helpers";
 
-export default class View{
-    constructor(){
+export default class View {
+  constructor(hangman) {
+    this.hangman = hangman;
+    this.tile_container = getElement("tile-container");
+    this.tiles = [];
+  }
 
+  // Get length of the word
+  getWord() {
+    return "banana";
+  }
+
+  createTiles() {
+    const word = this.getWord();
+    for (let index = 0; index < word.length; index++) {
+      const tile = createElement("div", "tile-container__tile");
+      tile.dataset.letter = word[index];
+      this.tile_container.appendChild(tile);
+      this.tiles.push(tile);
     }
+  }
 
-
+  //! Fix variable assignment
+  addLetters(letter) {
+    this.tiles.forEach((tile) => {
+      if (tile.dataset.letter === letter) tile.textContent = letter;
+    });
+  }
 }
